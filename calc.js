2de00a345b9c3b510fb0cdf3eceee00d12fc2b64
Zer0th <274134010+absolute-zer0th@users.noreply.github.com>
2026-07-01 11:44:57 +0300

    Made calc (slang for calculator) UI

--- calc.js
+++ calc.js
@@ -1,3 +1,7 @@
+const calc = document.querySelector("#calculator");
+const disp = document.querySelector("#display");
+const cont = document.querySelector("#container");
+
 function operate(
   a = prompt("Enter the first number:"),
   o = prompt("Enter the operator (+, -, *, /):"),
@@ -16,5 +20,3 @@ function operate(
       return NaN;
   }
 }
-
-console.log(operate());
